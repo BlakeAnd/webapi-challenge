8f7f8a3c8de1935ffffbdf937d24819974ede215
Blake Mitchell <mblakeandrew@gmail.com>
2019-07-12 09:59:21 -0700

    finished crud functionality for projects

---
+++ routers/actionRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const actionDB = require("../data/helpers/actionModel.js");
+const projectDB = require("../data/helpers/projectModel.js");
+
+
+
+function checkProjectId(req, res, next) {
+
+}
+module.exports = router;
